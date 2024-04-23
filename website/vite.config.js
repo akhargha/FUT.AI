f6acc33cb49b307541cedbc1d8e40b9ee0d3be32
Anupam Khargharia <kharghariaanupam07@gmail.com>
2024-04-23 16:06:25 -0400

    Dockerfile website vite

--- website/vite.config.js
+++ website/vite.config.js
@@ -13,4 +13,12 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
 });
