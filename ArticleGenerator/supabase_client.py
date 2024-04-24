9df5df3a3577f9c78f0a54da2c080fad26e0a5b3
Shivanshu <shivanshu.dwivedi@trincoll.edu>
2024-04-23 20:22:00 -0400

    Pushing the Dockerfiles

--- ArticleGenerator/supabase_client.py
+++ ArticleGenerator/supabase_client.py
@@ -1,5 +1,9 @@
 import os
+
+from flask.cli import load_dotenv
 from supabase import create_client, Client
+
+load_dotenv()
 
 url: str = os.environ.get('SUPABASE_URL')
 key: str = os.environ.get('SUPABASE_KEY')
